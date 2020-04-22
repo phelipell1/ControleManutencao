210081b47f3ef445360b909296124a1878c2a8a6
Phelype Rodrigo <phelipell1@gmail.com>
2020-04-22 09:11:51 -0300

    Modificado a função para adicionar ao banco de dados

--- forms/cadastro_remessa.php
+++ forms/cadastro_remessa.php
@@ -19,6 +19,21 @@ $link = $ObjDB->connecta_mysql();
                 }
             });
         })
+        //Essa parte irá buscar os destinatários com base na Cidade.
+        $("#cid_Codigo").change(function() {
+            var valor = $('#cid_Codigo option:selected').val();
+            $.ajax({
+                url: '../controllers/busca_destinatarios.php',
+                method: 'post',
+                data: {
+                    cid: $('#cid_Codigo').val()
+                },
+                success: function(data) {
+                    $('#cbmAcDestinatario').html(data).show();
+                }
+            });
+        })
+
 
         $('#btn_Cadastrar').click(function() {
             $.ajax({
@@ -48,79 +63,85 @@ $link = $ObjDB->connecta_mysql();
 <div class="container">
     <div class="shadow-lg mb-5 p-3 rounded border">
         <form id="form_Remessa">
-            <div class="row col">
-
-                <div class="form-group col-2">
-                    <label for="rem_Remetente">Remetente</label>
-                    <select name="rem_Remetente" id="rem_Remetente" class="form-control form-control-sm">
-                        <option selected>Selecione</option>
-                        <?
-                        $query_rem = "select * from Remetente";
-                        $result_rem = mysqli_query($link, $query_rem);
-                        $rows_rem = mysqli_num_rows($result_rem);
-                        foreach ($result_rem as $rows_rem) {
-                            echo '<option  value="' . $rows_rem['rem_Codigo'] . '">' . $rows_rem['rem_Abreviado'] . '</option>';
-                        }
-                        ?>
-                    </select>
+            <div class="row">
+                <div class="col-sm-5">
+                    <fieldset class="border">
+                        <legend>
+                            <h6>Remetente</h6>
+                        </legend>
+                        <div class="row col">
+                            <div class="form-group col">
+                                <label for="cbmEmpresa">Empresa</label>
+                                <select name="cbmEmpresa" id="cbmEmpresa" class="form-control form-control-sm">
+                                    <option selected></option>
+                                    <?
+                                    $query_tmp = "select * from Empresa";
+                                    $result_tmp = mysqli_query($link, $query_tmp);
+                                    $rows_tmp = mysqli_num_rows($result_tmp);
+                                    foreach ($result_tmp as $rows_tmp) {
+                                        echo '<option value="' . $rows_tmp['emp_Codigo'] . '">' . $rows_tmp['emp_RasaoSocial'] . '</option>';
+                                    }
+                                    ?>
+                                </select>
+                            </div>
+                            <div class="form-group col">
+                                <label for="cmbExpeditor">Expeditor</label>
+                                <select name="cmbExpeditor" id="cmbExpeditor" class="form-control form-control-sm">
+                                    <option selected></option>
+                                    <?
+                                    $query_tmp = "select * from Remetente";
+                                    $result_tmp = mysqli_query($link, $query_tmp);
+                                    $rows_tmp = mysqli_num_rows($result_tmp);
+                                    foreach ($result_tmp as $rows_tmp) {
+                                        echo '<option value="' . $rows_tmp['rem_Codigo'] . '">' . $rows_tmp['rem_Abreviado'] . '</option>';
+                                    }
+                                    ?>
+                                </select>
+                            </div>
+                        </div>
+                    </fieldset>
                 </div>
-                <div class="form-group col-2">
-                    <label for="rem_Destinatario">Destinatário</label>
-                    <select name="rem_Destinatario" id="rem_Destinatario" class="form-control form-control-sm">
-                        <option selected>Selecione</option>
-                        <?
-                        $query_des = "select * from Destinatario";
-                        $result_des = mysqli_query($link, $query_des);
-                        $rows_des = mysqli_num_rows($result_des);
-                        foreach ($result_des as $rows_des) {
-                            echo '<option  value="' . $rows_des['cod_Destinatario'] . '">' . $rows_des['desc_AcAbreviado'] . '</option>';
-                        }
-                        ?>
-                    </select>
+                <div class="col-sm-7">
+                    <fieldset class="border">
+                        <legend>
+                            <h6>Destinatario</h6>
+                        </legend>
+                        <div class="row col">
+                            <div class="form-group col-3">
+                                <label for="est_Codigo">UF</label>
+                                <select name="est_Codigo" id="est_Codigo" class="form-control form-control-sm">
+                                    <option selected>-Selecione-</option>
+                                    <?
+                                    $query_uf = "select * from UF";
+                                    $result_uf = mysqli_query($link, $query_uf);
+                                    $rows_uf = mysqli_num_rows($result_uf);
+                                    foreach ($result_uf as $rows_uf) {
+                                        echo '<option value="' . $rows_uf['codUf'] . '">' . $rows_uf['UF'] . '</option>';
+                                        $uf = $rows_uf['codUf'];
+                                    }
+                                    ?>
+                                </select>
+                            </div>
+                            <div class="form-group col">
+                                <label for="cid_Codigo">Cidade</label>
+                                <select name="cid_Codigo" id="cid_Codigo" class="form-control form-control-sm">
+                                    <option selected>-Selecione-</option>
+                                </select>
+                            </div>
+                            <div class="form-group col">
+                                <label for="cbmAcDestinatario">Aos Cuidados</label>
+                                <select name="cbmAcDestinatario" id="cbmAcDestinatario" class="form-control form-control-sm">
+                                    <option selected>-Selecione-</option>
+                                </select>
+                            </div>
+                        </div>
+                    </fieldset>
                 </div>
-                <div class="form-group col-2">
-                    <label for="est_Codigo">UF</label>
-                    <select name="est_Codigo" id="est_Codigo" class="form-control form-control-sm">
-                        <option selected>Selecione</option>
-                        <?
-                        $query_uf = "select * from UF";
-                        $result_uf = mysqli_query($link, $query_uf);
-                        $rows_uf = mysqli_num_rows($result_uf);
-                        foreach ($result_uf as $rows_uf) {
-                            echo '<option  value="' . $rows_uf['codUf'] . '">' . $rows_uf['UF'] . '</option>';
-                        }
-                        ?>
-                    </select>
-                </div>
-                <div class="form-group col-4" id="cid">
-                    <label for="cid_Codigo">Destino</label>
-                    <select name="cid_Codigo" id="cid_Codigo" class="form-control form-control-sm">
-                        <option selected>Selecione</option>
-                    </select>
-                </div>
-                <div class="form-group col-2">
-                    <label for="temp_Empresa">Envio</label>
-                    <select name="temp_Empresa" id="temp_Empresa" class="form-control form-control-sm">
-                        <option selected>Selecione</option>
-                        <?
-                        $query_tmp = "select * from Empresa";
-                        $result_tmp = mysqli_query($link, $query_tmp);
-                        $rows_tmp = mysqli_num_rows($result_tmp);
-                        foreach ($result_tmp as $rows_tmp) {
-                            echo '<option value="' . $rows_tmp['emp_Codigo'] . '">' . $rows_tmp['emp_RasaoSocial'] . '</option>';
-                        }
-                        ?>
-                    </select>
-                </div>
-                <div class="form-group col-12">
-                    <label for="rms_Descricao">Descrição</label>
-                    <textarea name="rms_Descricao" id="rms_Descricao" cols="3" rows="3" class="form-control "></textarea>
-                </div>
-                <div class="form-group col-12">
-                    <button type="button" class="btn btn-sm btn-success" id="btn_Cadastrar">Cadastrar</button>
-                    <button type="button" class="btn btn-sm btn-success" id="btn_Concluir">Concluir</button>
-                    <button type="reset" class="btn btn-sm btn-success">Limpar</button>
-
+                <div class="form-group col">
+                    <br>
+                    <button type="button" class="btn btn-primary" id="btn_Cadastrar">Cadastrar</button>
+                    <button type="button" class="btn btn-success" id="btn_Concluir">Concluir</button>
+                    <button type="reset" class="btn btn-warning">Limpar</button>
                 </div>
             </div>
         </form>
